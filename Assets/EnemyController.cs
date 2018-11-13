1741f1491987ab6bd54909ec16740e311528dc77
EmilianoCervantes <ovman96@hotmail.com>
2018-11-12 20:41:02 -0600

    Lo que tenía Alfredo

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+
+    public Transform target;
+    
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>().SetDestination(target.position);
+        gameObject.GetComponent<Animator>().SetBool("avanza", true);
+	}
+}
