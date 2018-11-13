54021b0805d754786464e0d7a5ef7ae253050f21
EmilianoCervantes <ovman96@hotmail.com>
2018-11-12 21:23:16 -0600

    Video de miedo cuando pierdes y ajuste en vel jugador

---
+++ Assets/Scripts/VideoController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour
+{
+	[SerializeField] private GameObject _vidObj;
+	
+	[SerializeField] private RawImage _video;
+	[SerializeField] private VideoPlayer _videoPlayer;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		StartCoroutine(PlayVideo());
+		StartCoroutine(HideVideo());
+	}
+
+	IEnumerator PlayVideo()
+	{
+		_videoPlayer.Prepare();
+		WaitForSeconds waitForSeconds = new WaitForSeconds(1);
+		while (!_videoPlayer.isPrepared)
+		{
+			yield return waitForSeconds;
+			break;
+		}
+
+		_video.texture = _videoPlayer.texture;
+		_videoPlayer.Play();
+	}
+
+	IEnumerator HideVideo()
+	{
+		WaitForSeconds waitForSeconds = new WaitForSeconds(2);
+		yield return waitForSeconds;
+		_vidObj.SetActive(false);
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Juego de laberinto con zombies en Unity
+
+## Encontrar la bandera/meta para escapar del laberinto.
