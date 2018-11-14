e661d015ab6bd34673296da65bc288f9d74a1a23
Alfredo Puente <alfredopv95@gmail.com>
2018-11-14 00:32:32 -0600

    Ya casi, solo falta el algoritmo

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BulletController : MonoBehaviour {
+
+    public float velocidad;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.forward * velocidad * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            SceneManager.LoadScene("gameover", LoadSceneMode.Single);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/TurretController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretController : MonoBehaviour {
+
+    public BulletController bala;
+    public Transform puntoDisparo;
+    public float tiempoDeEspera;
+    private float contador;
+
+	// Use this for initialization
+	void Start () {
+        contador = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(contador == 0)
+        {
+            BulletController nuevaBala = Instantiate(bala, puntoDisparo.position, puntoDisparo.rotation) as BulletController;
+            contador = tiempoDeEspera;
+            Debug.Log("Disparo");
+            Debug.Log(contador);
+        }
+        else
+        {
+            contador--;
+        }
+	}
+}
